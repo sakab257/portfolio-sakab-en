fbf2d9bed910adca521e43f2245aaa6704a442cb
sakab257 <slmbouskine93@gmail.com>
2025-10-11 19:49:41 +0200

    Commencement de la page Experiences

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,8 +1,19 @@
+import Card from '@/components/experiences/card'
+import ProjectList from '@/components/projets/project-list'
 import React from 'react'
 
 const Experiences = () => {
   return (
-    <div>Experiences</div>
+    <div className='w-full h-full'>
+      <div className='w-full flex flex-col justify-center items-center mx-auto gap-4'>
+        <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold text-center w-[250px] md:w-[400px] xl:w-[800px]">Mon <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">Parcours</span> Professionnel</h1>
+        <div className='w-full h-full flex flex-wrap justify-center gap-2 p-4'>
+          <Card icon='/icons/icon1.svg' title='Stagiaire Concepteur Développeur' description={`
+            Participé au développement d'une plateforme GraphQL/Drupal avec interfaces Next.js, conteneurisation Docker et automatisation CI/CD via GitHub Actions/Vercel, améliorant performance et déploiement.
+            `} />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -1,4 +1,3 @@
-import ProjectCard from '@/components/projets/project-card'
 import ProjectList from '@/components/projets/project-list'
 import React from 'react'
 
@@ -7,7 +6,7 @@ const Projets = () => {
     <div className='w-full h-full'>
       <div className='w-full flex flex-col justify-center items-center mx-auto gap-4'>
         <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold text-center w-[250px] md:w-[400px] xl:w-[800px]">Tous mes <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">projets</span> réalisés au cours de ma <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">carrière</span></h1>
-        <div className='w-full h-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-2 p-2'>
+        <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-2 gap-y-4 p-2'>
           <ProjectList />
         </div>
       </div>
